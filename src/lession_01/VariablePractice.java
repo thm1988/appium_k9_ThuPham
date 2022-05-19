42c81c055e92469d6a555212f3d439329284b45d
ThuPham <ptthu36@gmail.com>
2022-05-19 18:08:02 +0700

    Add Variable Java file

---
+++ src/lession_01/VariablePractice.java
@@ -0,0 +1,15 @@
+package lession_01;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        byte myNewByte = Byte.MAX_VALUE;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(myNewByte);
+
+        float myFloatNumber = 9.8F;
+        double myDoubleNumber = 7.6D;
+        System.out.println(myFloatNumber);
+        System.out.println(myDoubleNumber);
+
+    }
+}
