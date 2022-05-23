5b243de2acdcc5d758f16300c7bd06635c50d2ed
ThuPham <ptthu36@gmail.com>
2022-05-23 16:22:41 +0700

    Update lab_2.1 & Add new file lab_2.3

--- src/lab_02/CalculateBMIWithAdvice.java
+++ src/lab_02/CalculateBMIWithAdvice.java
@@ -2,7 +2,7 @@ package lab_02;
 
 import java.util.Scanner;
 
-public class CalculateBMI {
+public class CalculateBMIWithAdvice {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,15 +22,24 @@ public class CalculateBMI {
 
         if (BMIResult < 18.5) {
             System.out.println("You are underweight");
+            float underWeight = 18.5f * 2 * userHeight;
+            float weightDifference = underWeight - userWeight;
+            System.out.printf("You should increase %f kg \n",weightDifference);
             }
         else if (BMIResult <= 24.9) {
             System.out.println("You are normal weight");
         }
         else if (BMIResult <= 29.9) {
             System.out.println("You are overweight");
+            float overWeight = 29.9f * 2 * userHeight;
+            float weightDifference = userWeight - overWeight;
+            System.out.printf("You should reduce %f kg \n",weightDifference);
         }
         else {
             System.out.println("You got obesity");
+            float obesityWeight = 30f * 2 * userHeight;
+            float obesityDifference = userWeight - obesityWeight;
+            System.out.printf("You should reduce %f kg \n",obesityDifference);
         }
     }
 
