b476f719170c3c348facb8d0247fb0e003196581
Thu Pham <ptthu36@gmail.com>
2022-05-31 06:11:58 +0700

    Add new file src/lab_04/ArrayListMenu.java

--- src/lab_04/ArrayListMenu.java
+++ src/lab_04/ArrayListMenu.java
@@ -1,5 +1,8 @@
 package lab_04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArrayListMenu {
@@ -12,10 +15,42 @@ public class ArrayListMenu {
         System.out.println("4. Get minimum number");
         System.out.println("0. Exit");
     }
+    public static void addNumberIntoArrayList(List<Integer> userListArg){
+        System.out.print("Please help to input your number \t");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        userListArg.add(userInput);
+    }
+    private static void printNumberFromArraylist(List<Integer> userListArg) {
+        System.out.println(Arrays.toString(userListArg.toArray()));
+    }
+
+    private static int getMaximumNumberFromArrayList(List<Integer> userListArg) {
+        int lengthList = userListArg.size();
+        int maxValue = userListArg.get(0);
+        for (int index = 0; index < lengthList; index++) {
+            if (userListArg.get(index) > maxValue){
+                maxValue = userListArg.get(index);
+            }
+        }
+        return maxValue;
+    }
+
+    private static int getMinimumNumberFromArrayList(List<Integer> userListArg) {
+        int lengthList = userListArg.size();
+        int minValue = userListArg.get(0);
+        for (int index = 0; index < lengthList; index++) {
+            if (userListArg.get(index) < minValue){
+                minValue = userListArg.get(index);
+            }
+        }
+        return minValue;
+    }
 
     public static void main(String[] args) {
         int userChoice = 0;
-        boolean flag_exit = true;
+        boolean flag_not_exit = true;
+        List<Integer> userArrayList = new ArrayList<>();
         do {
             printMenu();
             Scanner scanner = new Scanner(System.in);
@@ -24,23 +59,34 @@ public class ArrayListMenu {
             userChoice = scanner.nextInt();
             switch (userChoice) {
                 case 1:
-                    System.out.println("Đây là số 1");
+                    System.out.println("1. Add number into ArrayList : ");
+                    addNumberIntoArrayList(userArrayList);
                     break;
                 case 2:
-                    System.out.println("Đây là số 2");
+                    System.out.println("2. Print numbers : ");
+                    printNumberFromArraylist(userArrayList);
                     break;
                 case 3:
-                    System.out.println("Đây là số 3");
+                    System.out.println("3. Get maximum number");
+                    int maxValueFromList=getMaximumNumberFromArrayList(userArrayList);
+                    System.out.printf("Max value from list is : %d \n",maxValueFromList);
                     break;
                 case 4:
-                    System.out.println("Đây là số 4");
+                    System.out.println("3. Get minimum number");
+                    int minValueFromList=getMinimumNumberFromArrayList(userArrayList);
+                    System.out.printf("Min value from list is : %d \n",minValueFromList);
                     break;
+
                 case 0:
                     System.out.println("You choose option 0");
-                    flag_exit=false;
+                    flag_not_exit=false;
                 default:
                     continue;
             }
-        } while (flag_exit);
+        } while (flag_not_exit);
     }
+
+
+
+
 }
