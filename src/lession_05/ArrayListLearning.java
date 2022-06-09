c19c61ecb56a393d034d09b81d36e5661db1d72c
ThuPham <ptthu36@gmail.com>
2022-06-09 18:16:44 +0700

    Update lession 5

--- src/lession_05/ArrayListLearning.java
+++ src/lession_05/ArrayListLearning.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class ArrayListLearning {
     public static void main(String[] args) {
         // Declare a List to contain integer value
-        List<Integer> userArrayList = Arrays.asList(1, 5, 6,8);
+        List<Integer> userArrayList = Arrays.asList(1, 5, 6, 8);
 
         for (Integer integer : userArrayList) {
-            
+            if(integer % 2 != 0){
+                integer = integer + 1;
+            }
         }
-
+        System.out.println(userArrayList);
 
     }
 }
