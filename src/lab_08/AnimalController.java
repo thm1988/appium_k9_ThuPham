224900a4a73ad882d62e5bad2fa8a49210dfc35f
ThuPham <ptthu36@gmail.com>
2022-06-20 19:20:08 +0700

    Update exercices 8

--- src/lab_08/AnimalController.java
+++ src/lab_08/AnimalController.java
@@ -15,7 +15,29 @@ public class AnimalController {
         Animal rooster = builder.setName("Rooster").setAnimalSpeed(15).setFlyAble(true).build();
 
         List<Animal> animalList = new ArrayList<>(Arrays.asList(tiger,dog,horse,eagle,rooster));
-        System.out.println("List of all animals: " + animalList);
+        List<Animal> animalListWithoutWings = new ArrayList<>();
+        System.out.println("Default list of animals: "+ animalList);
+
+        // Create a list of non-flyable animal
+        for (Animal animal : animalList) {
+            if (!animal.isFlyAble()){
+                animalListWithoutWings.add(animal);
+            }
+        }
+        System.out.println("Default list of non-flyable animals: "+ animalListWithoutWings);
+        // Back to lab 07_02 | Create a hashmap with key = Speed /value= Animal and add data into it
+        
+        Map<Integer, List<Animal>> animalDataRecord = new HashMap<>();
+
+        for (Animal animal : animalListWithoutWings) {
+            if(animalDataRecord.containsKey(animal.getAnimalSpeed())){
+                    animalDataRecord.get(animal.getAnimalSpeed()).add(animal);
+            } else
+                animalDataRecord.put(animal.getAnimalSpeed(), new ArrayList(Arrays.asList(animal)));
+        }
+
+        System.out.println(animalDataRecord.keySet());
+
 
     }
 
